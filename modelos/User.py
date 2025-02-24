4461d4058a521fdaa4bebd12b4710dfbc6d6d451
MartiniGitsi <martin.delrio2000@gmail.com>
2025-02-23 19:56:16 -0600

    Tabla nueva2

---
+++ modelos/User.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, DateTime, String, Integer, func
+from database import Base  # Import Base from the database module
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
